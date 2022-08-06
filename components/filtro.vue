25fa8e21cfdf6465d9039a9bf9f3c3ffe3afc250
AndrewsDutraDev <dutrapgmdr@gmail.com>
2022-08-06 20:18:04 -0300

    Add: filter

---
+++ components/filtro.vue
@@ -0,0 +1,96 @@
+<template>
+  <b-container class="filtro">
+    <b-form @submit="onSubmit">
+      <b-row>
+        <b-col sm="12" md="6">
+          <b-form-group
+            id="titulo"
+            label="Título"
+            label-for="titulo"
+          >
+            <b-form-input
+              id="titulo"
+              type="text"
+              v-model="form.title"
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group
+            id="centro"
+            label="Centro"
+            label-for="centro"
+          >
+            <b-form-input
+              id="centro"
+              type="text"
+              v-model="form.center"
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group id="area-tematica" label="Área temática principal" label-for="area-tematica">
+            <b-form-select
+              id="area-tematica"
+              v-model="form.main_area"
+              :options="main_area"
+            ></b-form-select>
+          </b-form-group>
+        </b-col>
+        <b-col sm="12" md="6">
+          <b-form-group id="modalidade" label="Modalidade" label-for="modalidade">
+            <b-form-select
+              id="modalidade"
+              v-model="form.modality"
+              :options="modality"
+            ></b-form-select>
+          </b-form-group>
+
+          <b-form-group id="unidade" label="Unidade" label-for="unidade">
+            <b-form-select
+              id="unidade"
+              v-model="form.unity"
+              :options="unity"
+            ></b-form-select>
+          </b-form-group>
+
+          <b-form-group id="second_area" label="Área temática secundária" label-for="second_area">
+            <b-form-select
+              id="second_area"
+              v-model="form.second_area"
+              :options="second_area"
+            ></b-form-select>
+          </b-form-group>
+        </b-col>
+        <b-col sm="12">
+          <b-button type="submit" class="btn">Consultar</b-button>
+        </b-col>
+      </b-row>
+    </b-form>
+  </b-container>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        form: {
+          title: '',
+          center: '',
+          main_area: '',
+          second_area: '',
+          modality: '',
+          unity: ''
+        },
+        main_area: [{ text: 'Selecione', value: null }, 'Meio ambiente', 'Direitos humanos e justiça'],
+        second_area: [{ text: 'Selecione', value: null }, 'Meio ambiente', 'Direitos humanos e justiça'],
+        modality: [{ text: 'Selecione', value: null }, 'Evento', 'Curso', 'Projeto', 'Programa'],
+        unity: [{ text: 'Selecione', value: null }, 'C3'],
+      }
+    },
+    methods: {
+      onSubmit(event) {
+        event.preventDefault()
+        alert(JSON.stringify(this.form))
+      },
+    }
+  }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
 <template>
-
+  <b-container class="main-page">
+    <Filtro/>
+  </b-container>
 </template>
 
 <script>
+import Filtro from '../components/filtro.vue';
 export default {
-
+  components:{
+    Filtro
+  }
 }
 </script>
