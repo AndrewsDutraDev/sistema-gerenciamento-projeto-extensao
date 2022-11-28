5647a717882943556f978464478068c86bc87b45
AndrewsDutraDev <dutrapgmdr@gmail.com>
2022-11-28 00:57:08 -0300

    TEst

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -3,7 +3,10 @@ export default function ({store, redirect, route, $cookies}) {
   console.log(store.getters.role)
   const cookieRes = $cookies.get('auth')
   console.log(cookieRes)
+  store.commit('setAuth', auth)
   console.log('dentro do middleware')
+  console.log(store.getters.isAuth)
+  console.log('-------')
   if (!store.getters.isAuth) {
     return redirect('/login')
   }
